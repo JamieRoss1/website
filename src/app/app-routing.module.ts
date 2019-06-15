e1c8c44c847225d9e485879a0d182f429b52c25f
JamieRoss1 <jamie.d.ross@googlemail.com>
2019-06-15 13:52:25 +0100

    Added header, footer, home and  blog components with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlogMainComponent } from './components/blog/blog-main/blog-main.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'blog',
+    component: BlogMainComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(
+        routes,
+        { enableTracing: true }
+      )
+  ],
+  exports: [
+    RouterModule
+  ]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/global/header/header.component';
+import { FooterComponent } from './components/global/footer/footer.component';
+import { BlogMainComponent } from './components/blog/blog-main/blog-main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    BlogMainComponent
   ],
   imports: [
     BrowserModule,
